21f6a05bfa6635f7600c58721719471d08f35b31
Or Hasson <hassonor@gmail.com>
2023-09-13 21:03:07 +0300

    add first dag example

---
+++ courses/learning_apache_ariflow/demo/dags/simple_hellow_world_ex.py
@@ -0,0 +1,24 @@
+from datetime import datetime, timedelta
+from airflow.utils.dates import days_ago
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+default_args = {
+    'owner': 'orhasson'
+}
+
+dag = DAG(
+    dag_id='hello_world',
+    description='"Hello World" DAG',
+    default_args=default_args,
+    start_date=days_ago(1),
+    schedule_interval=None
+)
+
+task = BashOperator(
+    task_id='hello_world_task',
+    bash_command='echo Hello world',
+    dag=dag
+)
+
+task
