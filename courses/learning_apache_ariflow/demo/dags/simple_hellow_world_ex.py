54c18cb7a2588ed505755eb4ab8bb952ed078490
Or Hasson <hassonor@gmail.com>
2023-09-13 21:06:28 +0300

    change to with DAG(..) as dag:

--- courses/learning_apache_ariflow/demo/dags/simple_hellow_world_ex.py
+++ courses/learning_apache_ariflow/demo/dags/simple_hellow_world_ex.py
@@ -7,18 +7,16 @@ default_args = {
     'owner': 'orhasson'
 }
 
-dag = DAG(
-    dag_id='hello_world',
-    description='"Hello World" DAG',
-    default_args=default_args,
-    start_date=days_ago(1),
-    schedule_interval=None
-)
+with DAG(
+        dag_id='hello_world',
+        description='"Hello World" DAG',
+        default_args=default_args,
+        start_date=days_ago(1),
+        schedule_interval=None
+) as dag:
+    task = BashOperator(
+        task_id='hello_world_task',
+        bash_command='echo Hello world',
+    )
 
-task = BashOperator(
-    task_id='hello_world_task',
-    bash_command='echo Hello world',
-    dag=dag
-)
-
-task
+    task
