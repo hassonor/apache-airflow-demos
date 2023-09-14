af6ee93f7128cea7df5eda3a08b1b271820c7815
Or Hasson <hassonor@gmail.com>
2023-09-14 19:26:22 +0300

    improve comments and remove null rows

--- courses/learning_apache_airflow/demo/dags/executing_python_pipeline_ex1.py
+++ courses/learning_apache_airflow/demo/dags/executing_python_pipeline_ex1.py
@@ -3,19 +3,20 @@ from airflow.utils.dates import days_ago
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 
-# Default arguments for the DAG
+# Default arguments to be used by the DAG
 default_args = {
     'owner': 'orhasson'
 }
 
 
-# Function to read a CSV file and print its contents.
-# The function now takes a file_path argument for flexibility.
+# Function to read a CSV file, print its contents, and return the data as a JSON string.
+# Accepts a filepath as an argument for better flexibility.
 def read_csv_file(file_path):
     """
-    Read a CSV file from the given path and print its contents.
+    Read a CSV file and return its contents as JSON.
+
     Args:
-    - file_path (str): Path to the CSV file to read.
+    - file_path (str): Path to the CSV file.
 
     Returns:
     - str: JSON representation of the DataFrame.
@@ -25,7 +26,27 @@ def read_csv_file(file_path):
     return df.to_json()
 
 
-# DAG definition
+# Function to remove any rows with null values from a DataFrame.
+# Uses XComs to retrieve the DataFrame from a previous task.
+def remove_null_values(**kwargs):
+    """
+    Remove rows with null values from the DataFrame and return the cleaned data as JSON.
+
+    Args:
+    - kwargs (dict): Contains runtime keyword arguments.
+
+    Returns:
+    - str: JSON representation of the cleaned DataFrame.
+    """
+    ti = kwargs['ti']
+    json_data = ti.xcom_pull(task_ids='read_csv_file')
+    df = pd.read_json(json_data)
+    df = df.dropna()
+    print(df)
+    return df.to_json()
+
+
+# Define the DAG
 with DAG(
         dag_id='executing_python_pipeline_ex1',
         description='Running a Python pipeline',
@@ -34,13 +55,18 @@ with DAG(
         schedule_interval='@once',
         tags=['python', 'transform', 'pipeline']
 ) as dag:
-    # PythonOperator to execute the read_csv_file function
-    # Now we pass the filepath as an argument to python_callable.
+    # Task to read the CSV file and convert it to JSON
     read_csv_file_task = PythonOperator(
         task_id='read_csv_file',
         python_callable=read_csv_file,
         op_args=['/opt/airflow/dags/datasets/insurance.csv']
     )
 
-# Task setting (though typically you'd have more tasks or dependencies)
-read_csv_file_task
+    # Task to remove rows with null values from the DataFrame
+    remove_null_values_task = PythonOperator(
+        task_id='remove_null_values_task',
+        python_callable=remove_null_values,
+    )
+
+# Set the task execution order
+read_csv_file_task >> remove_null_values_task
